5ba3799da717e264e86971505ee819a40dbbb3d2
PanMarcipan <barancikfilip@gmail.com>
2023-12-01 11:34:54 +0100

    Implementacia metod v API #15

--- src/orienter/communicator/api.py
+++ src/orienter/communicator/api.py
@@ -49,3 +49,101 @@ class API:
         response = requests.get(f"{self.api_endpoint}/competitions/{competition_id}", headers=self._get_auth_headers())
         self._handle_response(response)
         return response.json()
+
+
+    def competition_registrations(self, competition_id, club_id) -> Sequence[Mapping[str: Any]]:
+        '''
+        Returns a list of registrations for the specific competition for the given club.
+        :param competition_id:
+        :param club_id:
+        '''
+        response = requests.get(f"{self.api_endpoint}/competitions/{competition_id}/entries",
+                                params={"club_id": club_id}, headers=self._get_auth_headers())
+        self._handle_response(response)
+        return response.json()
+
+    def create_registration(self, competition_id, request_body: Mapping[str: Any]) -> Mapping[str: Any]:
+        '''
+        Registers a runner for a specific competition and returns entry ID and the runner's ID in the entry.
+        :param competition_id:
+        :param request_body:
+        '''
+        response = requests.post(f"{self.api_endpoint}/competitions/{competition_id}/entries/save",
+                                 json=request_body, headers=self._get_auth_headers())
+        self._handle_response(response)
+        return response.json()
+
+    def delete_runner(self, competition_id, request_body: Mapping[str: Any]):
+        '''
+        Deletes a runner from the race registration.
+        :param competition_id:
+        :param request_body:
+        '''
+        response = requests.post(f"{self.api_endpoint}/competitions/{competition_id}/entries/delete",
+                                 json=request_body, headers=self._get_auth_headers())
+        self._handle_response(response)
+
+    def event_results(self, competition_id, event_id) -> Sequence[Mapping[str: Any]]:
+        '''
+        Returns the list of results for a specific event of the competition.
+        :param competition_id:
+        :param event_id:
+        :return:
+        '''
+        response = requests.get(f"{self.api_endpoint}/competitions/{competition_id}/results/{event_id}",
+                                headers=self._get_auth_headers())
+        self._handle_response(response)
+        return response.json()
+
+    def runner_details(self, runner_id) -> Sequence[Mapping[str: Any]]:
+        '''
+        Returns details about the specified runner.
+        :param runner_id:
+        '''
+        response = requests.get(f"{self.api_endpoint}/runners/{runner_id}",
+                                headers=self._get_auth_headers())
+        self._handle_response(response)
+        return response.json()
+
+    def runner_results(self, runner_id, date_from, date_to) -> Sequence[Mapping[str: Any]]:
+        '''
+        Returns results achieved in the specified time window by the specified runner.
+        :param runner_id:
+        :param date_from:
+        :param date_to:
+        '''
+        response = requests.get(f"{self.api_endpoint}/runners/{runner_id}/results",
+                                params={"date_from" : date_from, "date_to" : date_to}, headers=self._get_auth_headers())
+        self._handle_response(response)
+        return response.json()
+
+    def registration_details(self, registration_id) -> Sequence[Mapping[str: Any]]:
+        '''
+        Returns detailed information about the registration.
+        :param registration_id:
+        '''
+        response = requests.get(f"{self.api_endpoint}/registrations/{registration_id}",
+                                headers=self._get_auth_headers())
+        self._handle_response(response)
+        return response.json()
+
+    def available_categories(self, registration_id, event_id) -> Sequence[int]:
+        '''
+        Returns a list of categories which are available to the runner.
+        :param registration_id:
+        :param event_id:
+        '''
+        response = requests.get(f"{self.api_endpoint}/registrations/{registration_id}/categories/{event_id}",
+                                headers=self._get_auth_headers())
+        self._handle_response(response)
+        return response.json()
+
+    def get_lists(self, list_id) -> Sequence[Mapping[str: Any]]:
+        '''
+        Returns the list of data from the selected directory
+        :param list_id:
+        '''
+        response = requests.get(f"{self.api_endpoint}/lists/{list_id}",
+                                headers=self._get_auth_headers())
+        self._handle_response(response)
+        return response.json()

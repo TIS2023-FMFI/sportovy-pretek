e33d65628fda8e27370bdf12e70cef97c87cdce1
David Krchňavý <david.krchnavy@gmail.com>
2023-12-08 13:46:31 +0100

    session.py: do not print on import

--- src/orienter/databasor/session.py
+++ src/orienter/databasor/session.py
@@ -2,14 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy import select
 from sqlalchemy.orm import sessionmaker
 from ..configurator import configuration
-from ..databasor import models
+from ..databasor.models import *
 
 engine = create_engine(f"sqlite:///{configuration.DATABASE_PATH}")
 Session = sessionmaker()
 Session.configure(bind=engine)
 session = Session()
 
-stmt = select(models.User)
+if __name__ == "__main__":
+    stmt = select(User).where(User.user_id == 1)
 
-for user in session.scalars(stmt):
-    print(user.first_name)
+    for user in session.scalars(stmt):
+        print(user.first_name, user.last_name)

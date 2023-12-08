770c8d30c109f53de106ca5538a8b93a1e35b8be
David Krchňavý <david.krchnavy@gmail.com>
2023-12-08 11:24:12 +0100

    orienter: remane configuration package

--- src/orienter/databasor/session.py
+++ src/orienter/databasor/session.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy import select
 from sqlalchemy.orm import sessionmaker
-from ..configuration import configuration
+from ..configurator import configuration
 from ..databasor import models
 
 engine = create_engine(f"sqlite:///{configuration.DATABASE_PATH}")
@@ -12,4 +12,4 @@ session = Session()
 stmt = select(models.User)
 
 for user in session.scalars(stmt):
-     print(user.first_name)
+    print(user.first_name)

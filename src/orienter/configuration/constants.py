e7a5b0e33f988dad4daecd6d7e0e40674ef63b37
David Krchňavý <david.krchnavy@gmail.com>
2023-12-08 09:20:46 +0100

    orienter: downgrade to python 3.8

--- src/orienter/configuration/constants.py
+++ src/orienter/configuration/constants.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from xdg_base_dirs import xdg_config_home, xdg_data_home
+from xdg import xdg_config_home, xdg_data_home
 
 CONFIG_FILE_PATH: Path = xdg_config_home() / "orienter.toml"
 XDG_DATA_HOME: Path = xdg_data_home()

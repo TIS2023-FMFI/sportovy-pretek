3172af44a10bde8cc892fd19471980bb6e78595a
David Krchňavý <david.krchnavy@gmail.com>
2024-01-31 13:57:00 +0100

    statistics_test.py: initial commit

---
+++ src/orienter/tester/statistics_test.py
@@ -0,0 +1,22 @@
+import unittest
+
+from ..statista.statistics import Generator, YEAR_AGO
+
+
+class StatisticsTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.generator = Generator()
+
+    def test_generate_one_racer(self):
+        with open('test_one_racer.html', 'w', encoding='UTF-8') as html:
+            html.write(self.generator.render([("Andrea", "Papugová")], since=YEAR_AGO))
+
+    def test_generate_multiple_racers(self):
+        with open('test_multiple_racers.html', 'w', encoding='UTF-8') as html:
+            html.write(self.generator.render([("Andrea", "Papugová"),
+                                              ("Karol", "Janšo"),
+                                              ("Peter", "Kotuliak"),
+                                              ("Andrej", "Mikloš"),
+                                              ("Maria", "Dubynets"),
+                                              ("Iveta", "Putnovská")], since=YEAR_AGO))
